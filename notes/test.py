df9b929ab1bef3caf0f7aad6314365a0bd8eb3b9
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-08-20 08:56:33 -0600

    asks about there day.

--- notes/test.py
+++ notes/test.py
@@ -2,4 +2,4 @@ print("test")
 print("Update")
 #print() <= EVerything inside of the parentheses is going to be printed
 name = input("What is your name?")
-print("Hello, "+ name)
+print("Hello, "+ name + "! How has your day been?")
