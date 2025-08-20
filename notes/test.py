438d37ac8a92cab8daa08ad106dd8a6a3ff97b6c
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-08-20 09:24:37 -0600

    update

--- notes/test.py
+++ notes/test.py
@@ -1,5 +1,2 @@
 print("test")
 print("Update")
-#print() <= EVerything inside of the parentheses is going to be printed
-name = input("What is your name?")
-print("Hello, "+ name + "! I hope you are haveing a great day!")
