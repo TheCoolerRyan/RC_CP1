ea7c4e663c757daead601f5e04a641ee9ddd6303
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-08-21 09:51:52 -0600

    Made notes on variables

---
+++ notes/variables.py
@@ -0,0 +1,7 @@
+#RC, 1st, Variable notes
+even_number = 2
+grade = 99
+animal = "Liger"
+print(grade)
+grade = 95
+print(grade)
---
+++ starters/seasons.py
@@ -0,0 +1,5 @@
+#RC, 1st, Seaseons
+
+last_name = input("What is your last name?")
+season = input("What is your favroite season?")
+print(f"Wow, {last_name} san, I like {season} too!")
