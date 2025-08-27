c0b0a46bd5aa785f6bddbdd6dd0fa5336a08b694
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-08-27 09:52:56 -0600

    Fixed all of my code and made a tic tac toe game

---
+++ fun_projects/rock_paper_scisors.py
@@ -0,0 +1,2 @@
+#For fun rock paper scissors!
+
---
+++ notes/numbers.py
@@ -0,0 +1,15 @@
+# RC, 1st, Numbers
+
+#Integer 
+#name = 999999999999999999999999999
+#social_security_number = 6666666666
+#motivation = -5
+#print(name, social_security_number, motivation)
+
+#Float
+#pi = 3.141592653589793238462643383279502884197169399375105820974944592307
+#print(pi)
+name = round(float(input("give number with a decimal:")))
+
+print(name)
+
