58b0f957a3ff0913501e20170a8c10c92116bf9c
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-08-29 09:53:36 -0600

    Made some tic tac toe

--- fun_projects/rock_paper_scisors.py
+++ fun_projects/rock_paper_scisors.py
@@ -1,2 +1,14 @@
 #For fun rock paper scissors!
-
+print("This is a program that mimics the game rock paper scissors! I will ask you for a number...")
+win = False
+import random
+while win == False:
+    random_int = random.randint(1,3)
+    if random_int == 1:
+        choice = "Rock"
+    elif random_int ==2:
+        choice = "Paper"
+    elif random_int ==3:
+        choice = "Scissors"
+    choice2 = int(input("Pick a number 1-3 (1 = , 2= ,3= !)"))
+    
---
+++ notes/strings.py
@@ -0,0 +1,4 @@
+#RC,1st, string notes
+string = input("Give me a sentance.")
+print(len(string))
+
