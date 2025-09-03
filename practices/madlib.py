65d35f84384091b68c1029e2f8c1f561695f1fb8
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-09-03 09:38:44 -0600

    Made a madlib

---
+++ practices/madlib.py
@@ -0,0 +1,7 @@
+#RC, 1st, Madlib
+adjective = input("Give me a adjective. (Don't have it end in ing. give me something like 'Fire')")
+noun = input("Give me a noun. (Not pluarl)")
+name = input("Give me a name.")
+a_room = input("Give me a room name.")
+adjective2 = input("Give me another adjective. (End in ing)")
+print("I woke up with a start and imediatly noticed the house was on " + adjective + " mode! Then I yelled 'Are you okay " + name + "?' I heard a response from " + name + " in the " + a_room + "! As I ran down the " + adjective2 + " house I stopped in shock. I looked at what I was seeing, and realized that there was a " + adjective2 + " " + noun + " in the " + a_room + "! That's when I realized it was all just a dream!")
