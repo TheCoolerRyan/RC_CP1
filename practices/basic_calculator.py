01a8902926455a5834de204dc30936c7aa2974ab
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-09-09 23:11:26 +0000

    Made it so it imediatly rounds to two decimal points.

--- practices/basic_calculator.py
+++ practices/basic_calculator.py
@@ -9,25 +9,25 @@ while start == True:
     equation = int(input("Pick the equation you want to do (1 = Addition, 2 = Subtraction, 3 = Multiplication, 4 = Division, 5 = Integer Division, 6 = Modulo, and 7 = Exponents)".strip()))
     if equation == 1:
         equal = number1 + number2
-        print(f"{number1} + {number2} = {equal}")
+        print(f"{number1} + {number2} = {equal:.2f}")
     if equation == 2:
         equal = number1 - number2
-        print(f"{number1} - {number2} = {equal}")
+        print(f"{number1} - {number2} = {equal:.2f}")
     if equation == 3:
         equal = number1 * number2
-        print(f"{number1} * {number2} = {equal}")
+        print(f"{number1} * {number2} = {equal:.2f}")
     if equation == 4:
         equal = number1 / number2
-        print(f"{number1} / {number2} = {equal}")
+        print(f"{number1} / {number2} = {equal:.2f}")
     if equation == 5:
         equal = number1 // number2
-        print(f"{number1} // {number2} = {equal}")
+        print(f"{number1} // {number2} = {equal:.2f}")
     if equation == 6:
         equal = number1 % number2
-        print(f"{number1} % {number2} = {equal}")
+        print(f"{number1} % {number2} = {equal:.2f}")
     if equation == 7:
         equal = number1 ** number2
-        print(f"{number1} ** {number2} = {equal}")
+        print(f"{number1} ** {number2} = {equal:.2f}")
     restart = int(input("Do you want to put them in another equation? Put 1 if yes, 2 if no.".strip()))
     if restart == 1:
         print("Okay, here we go again!")
