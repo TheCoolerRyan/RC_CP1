7fba60d6740128ea1c8e5718bfbc62f7fb4519af
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-09-10 09:50:31 -0600

    Fixed my crew

---
+++ projects/crew_shares.py
@@ -0,0 +1,8 @@
+#RC, 1st, Crew Shares
+
+print("After there adventure the crew earned a whole bunch of money on the last outing, but the captain didn't have time to divvy it all up before release everyone to port. He gave each member of the crew 500 dollars for the evening and then sat down with his first mate to properly divide the shares.")
+
+mon_earned = int(input(f"How much money did they get?".strip()))
+mon_earned = (f"{mon_earned:.2f}")
+
+crew_mem = int(input) #Finish this
