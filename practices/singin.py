51478844334e3262025b30190fd201955dd2a13f
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-09-22 09:44:44 -0600

    Made my program for signing in, going to learn for loops/dictionarys

--- practices/singin.py
+++ practices/singin.py
@@ -1,1 +1,16 @@
 #RC, 1st, User sign in
+username = "James123"
+password = "Clang"
+
+
+username1 = input("Give me your username.").strip()
+password1 = input("Give me your password.").strip()
+
+if username == username1 and password == password1:
+    print(f"Hello {username}, welcome back.")
+elif username == username1 and password != password1:
+    print("Your username is correct but your password is wrong.")
+elif username != username1 and password == password1:
+    print("Your username is incorrect but your password is wright.")
+else:
+    print("Wow, you got everything wrong... Thats kind of sad...")
