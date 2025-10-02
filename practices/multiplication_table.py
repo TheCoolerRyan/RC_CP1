3e8fde967b665d2f682bc37d657859a46f1eda60
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-10-02 09:34:34 -0600

    Made the mult table

--- practices/multiplication_table.py
+++ practices/multiplication_table.py
@@ -4,9 +4,9 @@ amount = int(input("How big do you want the multiplication level?"))
 mult = 1
 
 for num in range(1, amount+1):
+    nums = []
     for i in range(1,13):
-        nums = []
-        nums.insert(num*mult) #Fix
-        mult += 1
+        nums.append(i*mult) #Fix
+    mult += 1
     print(nums)
 
