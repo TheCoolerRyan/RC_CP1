56d7ddcf7089eaa686ed585e02945c63af1f2ce9
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-10-21 09:53:50 -0600

    Scrambled my ______ brain.

--- notes/function.py
+++ notes/function.py
@@ -1,8 +1,24 @@
 #RC, 1st, Function notes
+
 #We set variables first and the we define functions
-
-
+player_health = 100
+monster_health = 100
 #Parameters serve as variables that only serve inside the function
+
+
+
+def damage(amount,turn):
+    if turn == "player":
+        return monster_health - amount, player_health
+    else:
+        return monster_health, player_health - amount
+monster_health, player_health = damage(10, "player")
+print(f"Monster Health: {monster_health}")
+print(f"Player Health: {player_health}")
+
+
+
+
 def add(x,y):
     return x+y
 #Return takes whatever is in it and replaces the arguments/function calling
@@ -16,7 +32,8 @@ def initials(name):
 
     return initials
 
-
+print(initials("Tia LaRose"))
+print(initials("Franco Barboza"))
 
 #Arguments go inside the function calls
 #Serveds as the specific value for that specific time
@@ -28,5 +45,4 @@ while x < add(3,9):
     print("Duck")
     x +=1
 print("Goose!")
-
 
---
+++ practices/caesar_cypher.py
@@ -0,0 +1,8 @@
+#RC, 1st, Caeser Cypher
+
+print(f"a = {ord("a")}")
+print(f"97 = {chr(98)}")
+
+
+#Lowercase is 97 to 122
+#Uppercase is 65 to 90
