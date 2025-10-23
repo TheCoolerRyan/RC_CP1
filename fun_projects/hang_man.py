a80f30c9ca42a2feb7bcb6e5f7392c26305f9052
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-10-23 09:53:40 -0600

    Made hang man

---
+++ fun_projects/hang_man.py
@@ -0,0 +1,28 @@
+
+head = """    O""" #Create if statement to make it so head starts out as blank
+body = """    |           |
+    |           |
+    |           |
+"""
+body1 = """    |           |
+----|---        |
+    |           |
+"""
+#Make body two the full body with legs added
+man = (f"""
+    ____________
+    |           |
+    |           |
+{head}           |
+{body1}                |
+                |
+                |
+                |
+                |
+           _____|______
+
+
+
+
+""")
+print(man)
