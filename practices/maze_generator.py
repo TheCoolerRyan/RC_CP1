d886a80a2560b8c233c4cbd1d3c7aea521aa7cb6
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-10-31 09:53:14 -0600

    Started on the maze generator

---
+++ practices/maze_generator.py
@@ -0,0 +1,23 @@
+#RC, 1st, Ryan Crop
+import turtle
+import random
+
+#Figure out maze widths and height
+maze_width = 100
+maze_height = 100
+cell_width = 10
+cell_height = 10
+
+#set up maze screen
+maze = turtle.Screen()
+maze.setup()
+
+
+#Will be in a list that decides wether or not 
+    #Create thing that decieds wether or not to put a white spot or a line
+
+    #Create list
+
+        #Figure out how to make it so theres always a way to win
+        #Create start and stop spots
+
