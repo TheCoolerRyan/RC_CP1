82900391ed7dfb7401088375c1ccbee235b1aba8
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-10-31 18:18:00 +0000

    Made a program for maze

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -1,19 +1,46 @@
 #RC, 1st, Ryan Crop
 import turtle
 import random
-
 #Figure out maze widths and height
-maze_width = 100
-maze_height = 100
 cell_width = 10
 cell_height = 10
 
 #set up maze screen
 maze = turtle.Screen()
-maze.setup()
+maze.setup(100,100)
+maze.tracer(0)
 
-
+pen = turtle.Turtle()
+pen.speed(50)
+pen.hideturtle()
+pen.penup()
+pen.goto(0,40)
+pen.pendown()
+pen.goto(-40,40)
+pen.goto(-40,-40)
+pen.goto(0,-40)
+pen.penup()
+pen.goto(10,-40)
+pen.pendown()
+pen.goto(40,-40)
+pen.goto(40,40)
+pen.goto(10,40)
 #Will be in a list that decides wether or not 
+def posibilite(x,y,spotx,spoty):
+    spotx = [-30,-20,-10,0,10,20,30]
+    spoty = [-30,-20,-10,0,10,20,30]
+    for y in spoty:
+        rany = random.randint(1,2)
+        if rany == 1:
+            pen.penup()
+            pen.sety(spoty[y])
+            pen.pendown()
+            pen.forward(10)
+        else:
+            pass
+        for x in spotx: #Create program for x
+            random.randint(1,2)
+        
     #Create thing that decieds wether or not to put a white spot or a line
 
     #Create list
