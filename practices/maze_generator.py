745828f8735c2f13c527ec225205be6785a2f66b
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-11-02 22:26:14 +0000

    Set up the bass for the maze creation.

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -27,24 +27,33 @@ pen.goto(40,40)
 pen.goto(10,40)
 #Will be in a list that decides wether or not 
 def posibilite(x,y,spotx,spoty):
-    spotx = [-30,-20,-10,0,10,20,30]
-    spoty = [-30,-20,-10,0,10,20,30]
+    spotx = [-40,-30,-20,-10,0,10,20,30,40]
+    spoty = [-40,-30,-20,-10,0,10,20,30,40]
     for y in spoty:
         rany = random.randint(1,2)
         if rany == 1:
+            xy = -40
             pen.penup()
             pen.sety(spoty[y])
+            pen.setx(xy)
             pen.pendown()
             pen.forward(10)
+            xy += 10
         else:
+            xy += 10
             pass
         for x in spotx: #Create program for x
-            random.randint(1,2)
-        
-    #Create thing that decieds wether or not to put a white spot or a line
+            ranx = random.randint(1,2)
+            if ranx == 1:
+                pen.penup()
+                pen.setx(spotx[x])
+                pen.sety(spoty[y])
+                pen.pendown()
+                pen.left(90)
+                pen.forward(10)
+            else:
+                pass
 
-    #Create list
-
+print(posibilite("","","",""))
         #Figure out how to make it so theres always a way to win
-        #Create start and stop spots
 
