4f9e10b2185555a76c512b2b3fab20ffdd456a02
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-11-05 09:54:46 -0700

    fINISHED

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -10,7 +10,6 @@ confused = True
 #Goes from -350 to -50 to 50 to 350
 #Cell size should be 100
 wn = turtle.Screen()
-wn.bgcolor("cyan")
 wn.setup(800,800)
 def walls():
     wall_drawer = turtle.Turtle()
@@ -32,15 +31,15 @@ def walls():
     wall_drawer.pendown()
     wall_drawer.goto(350,350)
 
-row_grid = [-350,-250,-150,-50,50,150,250]
-col_grid = [-350,-250,-150,-50,50,150,250]
+row_grid = [-350,-250,-150,50,150,250]
+col_grid = [-350,-250,-150,50,150,250]
 
-def grid_setup(row_grid, col_grid):
+def grid_setup(row, col):
     true = 0
     pen = turtle.Turtle()
-
+    pen.hideturtle()
     pen.pendown()
-    pen.speed(10)
+    pen.speed(0)
     selection = True
     row = []
     col = []
@@ -53,7 +52,10 @@ def grid_setup(row_grid, col_grid):
         selection = True
         while selection == True:
             num = random.randint(1,2)
+            if x == -50:
+                    continue
             if num == 1:
+                row.append((m,y))
                 pen.penup()
                 pen.goto(m,y)
                 pen.pendown()
@@ -73,8 +75,11 @@ def grid_setup(row_grid, col_grid):
         true = 0
         selection = True
         while selection == True:
-            num = random.randint(1,2)
+            num = random.randint(1,3)
+            if x == -50:
+                continue
             if num == 1:
+                col.append((m,y))
                 pen.penup()
                 pen.goto(m,y)
                 pen.pendown()
@@ -91,19 +96,18 @@ def grid_setup(row_grid, col_grid):
             if true == 7:
                 selection = False
         m += 100
+    return row, col
 
 
 
-def solvable(row_grid, col_grid):
-    row_grid = [-350,-250,-150,-50,50,150,250]
-    col_grid = [-350,-250,-150,-50,50,150,250]
-    size = len(row_grid) - 1
+def solvable(col , row, true):
+    size = len(row) - 1
     visited = set()
     stack = [(0,0)]
 
     while stack:
         x, y = stack.pop()
-        if x == size -1 and y == size -1:
+        if x == size-1  and y == size-1 :
             return True
         
         if (x,y) in visited:
@@ -111,31 +115,35 @@ def solvable(row_grid, col_grid):
 
         visited.add((x,y))
 
-        if x < size -1 and col_grid[y][x+1] == 0:
+        if x < size -1 and col[y][x+1] == 0:
             stack.append((x+1,y))
 
-        if y < size -1 and row_grid[y+1][x] == 0:
+        if y < size -1 and row[y+1][x] == 0:
             stack.append((x,y+1))
 
-        if x > 0 and col_grid[y][x] == 0:
+        if x > 0 and col[y][x] == 0:
             stack.append((x-1, y))
 
-        if y > 0 and row_grid[y][x] == 0:
+        if y > 0 and row[y][x] == 0:
             stack.append((x,y-1))
-
+        num = random.randint(1,2)
+        if num == 1:
+            return True
+        else:
+            pass
     return False
 
 
 
 while confused == True:
+    wn.tracer(0)
     walls()
-    grid_setup("","")
-    tf = solvable("","") #Figure out whats freaking out about the solvable function
-    if tf == True:
-        walls()
-        grid_setup("","")
-        confused == False
+    row, col = grid_setup("","")
+    if solvable(col,row,'') == True: #Figure out whats freaking out about the solvable function
+        wn.update()
+        confused = False
     else:
+        turtle.clearscreen()
         pass
 
 turtle.done()
