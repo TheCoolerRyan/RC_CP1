92163f3176bbd33b8d6c2e809b5006fa4e5f350f
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-11-17 19:18:09 +0000

    Made my number chosing program

--- practices/flexible_calculator.py
+++ practices/flexible_calculator.py
@@ -3,5 +3,17 @@
 print("This is a Flexible Calculator that can calculate the sum, average, max, min,  and product of what you put in.")
 def nums():
     x = True
+    nums = []
     while x == True:
-        num = int(input("Pick a number."))
+        num = input("Pick a number. If you want to stop picking numbers, put no").strip().upper()
+        if num.isdigit() != True and num != "NO":
+            print("That is not a choice...")
+            pass
+        elif num == "NO":
+            x = False
+        else:
+            nums.append(num)
+        
+    return nums
+
+nums()
