9c42461ced3633f13ac35a9964d06a7a2dc137eb
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-11-25 09:53:42 -0700

    Added 80 lines of pseducode

---
+++ final/game.py
@@ -0,0 +1,80 @@
+#RC, 1st, Final game
+
+#set the list for beaten people
+#beaten = []
+#set stat dictionary
+#stat{ "charm": 8,
+    #  "wisdom": 8,
+    #  "strength": 8,
+    #  "win": 0,
+    #  "lose": 0,
+    #  "gold": 0}
+
+
+#Set functions
+
+# 1st function
+#def stat_increase(stat):
+    #if gain == crocs and stat[gold] > 1:
+        #stat[charm] -= 2
+        #stat[wisdom] -= 2
+        #stat[strength] += 4
+        #gold -=2 
+        #return stat
+    #elif gain == nerdy_glasses and stat[gold] > 1:
+        #stat[charm] -= 2
+        #stat[wisdom] += 4
+        #stat[strength] -= 2
+        #gold -=2 
+        #return stat
+    #elif gain == suit and stat[gold] > 1:
+        #stat[charm] += 4
+        #stat[wisdom] -= 2
+        #stat[strength] -= 2
+        #gold -=2 
+        #return stat
+    #elif gain == charm and stat[gold] > 1:
+        #Charm += 2
+        #Gold -= 2
+        #Return stat
+    #elif gain == wisdom and stat[gold] > 1:
+        #wisdom += 2
+        #Gold -= 2
+        #Return stat
+    #elif gain == strength and stat[gold] > 1:
+        #strength += 2
+        #Gold -= 2
+        #Return stat
+    #else:
+        #How did you manage this.
+
+
+
+# 2nd function
+#def lose(stat):
+    #if stat[lose] == 3:
+        #set end/quit to true
+    #else:
+        #set end/quit to false
+        #pass
+
+# 3rd function
+#def fight(stat, room)
+    #if room == 1:
+        #Print out options to buy and also the base stats
+        #Make sure not to allow them to buy the stats
+    #elif room == 2:
+        #if 2 isin't in beaten list:
+            #enemy = nerd
+            #write out all of the enemy stats here.
+
+
+            #Roll for who gets to decide the attack.
+            #if robotroll > human roll:
+                #attack = wisdom
+            #else:
+                #attack = user input()
+
+
+        #else:
+            #Print of thing telling the person this room has already been concured
